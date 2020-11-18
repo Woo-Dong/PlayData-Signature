3c0d14090c987068c481c2e1338f1cd860e4ce10
Woo-Dong <dwpark2007@naver.com>
2020-11-18 17:06:40 +0900

    1118 cron-job function updated

--- cron_job_server/functions.py
+++ cron_job_server/functions.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient 
+from fbprophet import Prophet
+import pandas as pd 
 import os
 
 
 def conn_db(): 
-    user = os.getenv("DBUSER", '')
-    pwd = os.getenv("DBPWD", '')
-    ip_addr = os.getenv("DBADDR", '')
+    user = os.getenv("DBUSER", 'signature')
+    pwd = os.getenv("DBPWD", 'shanekang')
+    ip_addr = os.getenv("DBADDR", '54.180.213.105')
 
     conn = MongoClient(f'mongodb://{user}:{pwd}@{ip_addr}:27017') 
     return conn
@@ -25,8 +27,28 @@ def insert_data(df, collection, *chk_keys):
     if doc_list: 
         collection.insert_many(doc_list)
         return True 
-
     return False 
+
+
+def train_FBProphet_model(data, 
+                        changepoint_prior_scale=.2,  changepoint_range=.98, 
+                        yearly_seasonality=False, weekly_seasonality=True, 
+                        daily_seasonality=True, seasonality_mode='additive'): 
+
+    prophet_df = pd.DataFrame(data, columns=['ds', 'y'])
+    prophet_df.sort_values('ds', inplace=True)
+
+    model = Prophet(
+        changepoint_prior_scale=changepoint_prior_scale, # increasing it will make the trend more flexible
+        changepoint_range=changepoint_range, # place potential changepoints in the 98% of the time series
+        yearly_seasonality=yearly_seasonality, 
+        weekly_seasonality=weekly_seasonality, 
+        daily_seasonality=daily_seasonality,
+        seasonality_mode=seasonality_mode)
+
+    model.fit(prophet_df) 
+
+    return model 
 
 # TODO 
 def upsert_data(df, collection, key, set_col): 
--- cron_job_server/inference_scripts.py
+++ cron_job_server/inference_scripts.py
@@ -1,65 +1,119 @@
+from functions import conn_db, insert_data, train_FBProphet_model
 
-from fbprophet import Prophet
 import pandas as pd 
 import os, io
 
-from functions import conn_db, insert_data 
 
-
-def predict_fbprophet(): 
+def predict_FBProphet_model(): 
 
     conn = conn_db() 
     domestic_cumul_collection = conn.DomesticCOVID.domestic_cumul
 
     total_data = [[elem['date'], elem['confirmed']] for elem in domestic_cumul_collection.find({})]
-
     conn.close() 
 
-    prophet_df = pd.DataFrame(total_data, columns=['ds', 'y'])
-    prophet_df.sort_values('ds', inplace=True)
+    prev_num = total_data[-1]
 
-    model = Prophet( 
-        changepoint_prior_scale=.2, # increasing it will make the trend more flexible
-        changepoint_range=.98, # place potential changepoints in the 98% of the time series
-        yearly_seasonality=False, 
-        weekly_seasonality=True, 
-        daily_seasonality=True,
-        seasonality_mode='additive'
-    )
-
-    model.fit(prophet_df) 
+    model = train_FBProphet_model(total_data) 
 
     future = model.make_future_dataframe(periods=7) 
     forecast = model.predict(future) 
+
+    forecast = forecast.iloc[-7:, :]
+
     forecast['yhat'] = forecast['yhat'].astype(int)
 
-    ret_df = forecast[['ds', 'yhat']]
+    ret_df = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
     ret_df.rename({'ds': 'date'}, axis=1, inplace=True) 
     ret_df['date'] = ret_df['date'].apply(lambda x:str(x).split()[0]) 
 
-    return ret_df
+    return ret_df, prev_num
+
+
+def validate_FBProphet_model(ADJ_CONST): 
+
+    conn = conn_db() 
+    domestic_cumul_collection = conn.DomesticCOVID.domestic_cumul 
+
+    total_data = [[elem['date'], elem['confirmed']] for elem in domestic_cumul_collection.find({})]
+    conn.close() 
+
+    train_data = total_data[:-7]
+    prev_num = total_data[-8][1] 
+
+    val_data = [(elem[1]-prev_num) for elem in total_data[-7:]]
+
+
+
+    model = train_FBProphet_model(train_data) 
+
+    future = model.make_future_dataframe(periods=7) 
+    forecast = model.predict(future) 
+
+    result = forecast.tail(7) 
+    col_list = ['ds', 'yhat', 'yhat_lower', 'yhat_upper']
+    result = result[col_list]
+
+    for col in col_list[1:]: result[col] = round((result[col] - prev_num) * ADJ_CONST, 0)
+    result['real'] = val_data 
+
+    return result 
+
+
+def insert_validate_FBProphet_data(ADJ_CONST=1.): 
+
+    try: 
+        val_df = validate_FBProphet_model(ADJ_CONST) 
+
+        conn = conn_db() 
+
+        validate_FBProphet_collection = conn.DomesticCOVID.validate_FBProphet 
+        validate_FBProphet_collection.drop()
+
+        col_list = val_df.columns 
+        doc_list = list() 
+
+        for _, row in val_df.iterrows(): 
+            doc = {k: max(v, 0) for k, v in zip(col_list[1:], row[1:])}
+            doc['date'] = str(row[0]).split()[0]
+            doc_list.append(doc)             
+
+        validate_FBProphet_collection.insert_many(doc_list) 
+        conn.close() 
+        return True 
+
+    except: 
+        print("Error Occured: insert_validate_FBProphet_data Function")
+        return False 
+
 
 def upsert_inference_data(): 
 
-    pred_df = predict_fbprophet()
+    pred_df, prev_num = predict_FBProphet_model()
     conn = conn_db()
-    inference_fbprophet_collection = conn.DomesticCOVID.inference_fbprophet
+    inference_FBProphet_collection = conn.DomesticCOVID.inference_FBProphet
+    col_list = pred_df.columns
 
     for _, row in pred_df.iterrows(): 
-        doc = {col: None for col in pred_df.columns} 
     
-        doc['date'] = row[0] 
-        doc['yhat'] = row[1]
-        inference_fbprophet_collection.find_one_and_update(
-                                            {'date': doc['date']}, 
-                                            {'$set': {'yhat': doc['yhat']}}, 
+        inference_FBProphet_collection.find_one_and_update(
+                                            {'date': row[0]}, 
+                                            {'$set': {
+                                                k: v for k, v in zip(col_list[1:], row[1:]) 
+                                                }}, 
                                             upsert=True)
 
+    prev_doc = { 
+        'date': prev_num[0], 
+        'real': prev_num[1]
+    }
+    inference_FBProphet_collection.insert(prev_doc) 
+                            
     conn.close() 
     return True
 
 
 if __name__ == "__main__":
-    print("Status: ", upsert_inference_data())
-     
+    print("predict FBProphet data Upsert: ", upsert_inference_data())
+    print("validate FBPropeht data Insert: ", insert_validate_FBProphet_data(ADJ_CONST=1.))
      
